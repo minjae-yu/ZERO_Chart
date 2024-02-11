e6f4920c5e3ecc2260a0886ecbe9fb4ae952d003
minjae-yu <minjae.yoo@digipen.edu>
2024-02-11 18:58:03 +0900

    Code Setting

--- ZERO_Chart/build.gradle.kts
+++ ZERO_Chart/build.gradle.kts
@@ -7,6 +7,13 @@ android {
     namespace = "com.example.zero_chart"
     compileSdk = 34
 
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+    }
+
     defaultConfig {
         minSdk = 24
 
@@ -33,11 +40,17 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.foundation:foundation:1.6.1")
+    implementation("androidx.compose.ui:ui:1.6.1")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
--- app/src/main/java/com/example/zero_chart/MainActivity.kt
+++ app/src/main/java/com/example/zero_chart/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.zero_chart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.zero_chart.ui.theme.ZERO_ChartTheme
 
@@ -22,25 +26,82 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        DrawLineChart(
+                            modifier = Modifier
+                                .background(Color.DarkGray)
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .weight(1f),
+                            lineData = listOf(
+                                lineData(listOf(100f, 40f, 20f, 80f, 60f), Color.Red, true),
+                                lineData(listOf(60f, 80f, 20f, 40f, 100f), Color.Blue, true),
+                                lineData(listOf(20f, 40f, 60f, 80f, 100f), Color.Green, true)
+                            ),
+                            verticalLines = 5,
+                            horizontalLines = 4,
+                            lineWidth = 3,
+                            isSmooth = true,
+                            isAnimation = true,
+                            animationDuration = 3000
+                        )
+
+                        DrawBarGraph(
+                            modifier = Modifier
+                                .background(Color.DarkGray)
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .weight(1f),
+                            data = listOf(100f, 200f, 50f, 80f),
+                            barWidth = 100f,
+                            barCornerRadius = 20f,
+                            horizontalLines = 4,
+                            isAnimation = true,
+                            animationDuration = 3000
+                        )
+
+                        DrawPieChart(
+                            modifier = Modifier
+                                .background(Color.DarkGray)
+                                .fillMaxWidth()
+                                .fillMaxHeight()
+                                .weight(1f),
+                            pieData = listOf(
+                                pieData(150, Color.Blue),
+                                pieData(120, Color.Cyan),
+                                pieData(110, Color.Red),
+                                pieData(170, Color.Green),
+                                pieData(120, Color.Yellow)
+                            ),
+                            radiusOuter = 90f,
+                            chartBarWidth = 90f,
+                            isAnimation = true,
+                            animationDuration = 1000
+                        )
+                    }
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PreviewLineChart() {
+    DrawLineChart(
+        modifier = Modifier
+            .background(Color.DarkGray)
+            .fillMaxWidth()
+            .fillMaxHeight(1 / 2f),
+        lineData = listOf(
+            lineData(listOf(100f, 40f, 60f, 20f, 80f), Color.Blue, true),
+            lineData(listOf(80f, 20f, 60f, 40f, 100f), Color.Red, true)
+        ),
+        verticalLines = 4,
+        horizontalLines = 4,
+        lineWidth = 3,
+        isSmooth = true,
+        isAnimation = true,
+        animationDuration = 3000
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ZERO_ChartTheme {
-        Greeting("Android")
-    }
 }
